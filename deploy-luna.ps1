903a74c6e09b3cb00b516617e8ad227e1deff34d
Harry <harrypierson@ngd.neo.org>
2020-10-27 17:48:54 -0700

    WIP

--- deploy-luna.ps1
+++ deploy-luna.ps1
@@ -2,4 +2,5 @@ dotnet run --project C:\Users\harry\Source\neo\seattle\express\src\neoxp\neoxp.c
 dotnet build
 dotnet run --project C:\Users\harry\Source\neo\seattle\express\src\neoxp\neoxp.csproj -- contract deploy ./luna-token/bin/Debug/netstandard2.0/LunaToken.avm owen
 dotnet run --project C:\Users\harry\Source\neo\seattle\express\src\neoxp\neoxp.csproj -- contract invoke ./luna-token/deploy.neo-invoke.json owen
+dotnet run --project C:\Users\harry\Source\neo\seattle\express\src\neoxp\neoxp.csproj -- transfer ./luna-token/bin/Debug/netstandard2.0/LunaToken.avm 100 owen alice
 dotnet run --project C:\Users\harry\Source\neo\seattle\express\src\neoxp\neoxp.csproj -- checkpoint create cp2 --force
